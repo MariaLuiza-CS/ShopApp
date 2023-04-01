b8ab7430ce66d04f8293d889b7548b24ce60cb3a
MariaLuiza-CS <m.luiza1843@gmail.com>
2023-04-01 18:55:27 -0300

    adding bottom navigation

--- app/src/main/java/com/example/shop/presentation/navigation/HomeNavigationGraph.kt
+++ app/src/main/java/com/example/shop/presentation/navigation/HomeNavigationGraph.kt
@@ -6,12 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.shop.Graph
-import com.example.shop.HomeScreen
 import com.example.shop.domain.utils.BottomNavItem
-import com.example.shop.domain.utils.NavGraph
-import com.example.shop.presentation.ui.screen.AnalyticsScreen
-import com.example.shop.presentation.ui.screen.ListScreen
-import com.example.shop.presentation.ui.screen.ProfileScreen
 import com.example.shop.presentation.ui.screen.ScreenContent
 
 @Composable
@@ -21,36 +16,20 @@ fun HomeNavigationGraph(navController: NavHostController) {
         route = Graph.HOME,
         startDestination = BottomNavItem.Home.title
     ) {
-        composable(route = BottomNavItem.Home.title){
+        composable(route = BottomNavItem.Home.title) {
             ScreenContent(
-                name = BottomNavItem.Home.title,
-                onClick = {
-                    Log.e("teste", "home screen")
-                }
             )
         }
-        composable(route = BottomNavItem.List.title){
+        composable(route = BottomNavItem.List.title) {
             ScreenContent(
-                name = BottomNavItem.Home.title,
-                onClick = {
-                    Log.e("teste", "home screen")
-                }
             )
         }
-        composable(route = BottomNavItem.Analytics.title){
+        composable(route = BottomNavItem.Analytics.title) {
             ScreenContent(
-                name = BottomNavItem.Home.title,
-                onClick = {
-                    Log.e("teste", "home screen")
-                }
             )
         }
-        composable(route = BottomNavItem.Profile.title){
+        composable(route = BottomNavItem.Profile.title) {
             ScreenContent(
-                name = BottomNavItem.Home.title,
-                onClick = {
-                    Log.e("teste", "home screen")
-                }
             )
         }
     }
